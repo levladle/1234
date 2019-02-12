1fd532ab5ccf1b01c9e8d7c6cdbd6e8be3a55a88
levladle <44490688+levladle@users.noreply.github.com>
2019-02-12 15:41:20 +0200

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,22 @@
+/*
+ * Decompiled with CFR 0_123.
+ * 
+ * Could not load the following classes:
+ *  Frame
+ */
+import java.awt.Rectangle;
+import javax.swing.JFrame;
+
+public class Frame {
+    public static JFrame frame;
+    public static Rectangle rect;
+
+    public Frame() {
+        throw new Error("Unresolved compilation problem: \n\tThe declared package \"src\" does not match the expected package \"\"\n");
+    }
+
+    public static void main(String[] arrstring) {
+        throw new Error("Unresolved compilation problem: \n");
+    }
+}
+
